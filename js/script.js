12c868f5476f8863979d71aa3ab9d1d1d9d21ebd
505kurt <fbmakurt@gmail.com>
2024-10-17 01:10:13 -0400

    multilinguagem & algumas alterações de qualidade do código
    commit lendario

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log("Hello World")
+function toggleDropdown() {
+    const dropdown = document.getElementById("lang-dropdown");
+    dropdown.classList.toggle("show");
+  }
+
+  // Fecha o dropdown se clicar fora dele
+  window.onclick = function(event) {
+    if (!event.target.closest('.button-dropdown')) {
+      const dropdowns = document.querySelectorAll(".dropdown-content");
+      dropdowns.forEach(dropdown => {
+        if (dropdown.classList.contains('show')) {
+          dropdown.classList.remove('show');
+        }
+      });
+    }
+  }
